c27af2e8bd5e20c31ebbcf18bc51fd9c9e0bf3cf
HiHi <3234252073@qq.com>
2024-03-20 08:53:32 +0800

    init

---
+++ src/Global_Parameter.py
@@ -0,0 +1,13 @@
+DIS_TO_COST = 1  # 送货车距离和代价的关系系数，距离 * 系数 = 代价
+DIS_TO_CHARGE = 1  # 距离和充电量的关系系数
+DIS_TO_CONSUME_OF_DELIVERY = 1  # 运输车辆距离和耗电量的关系系数
+DIS_TO_CONSUME_OF_CHARGE = 1  # 充电车距离和耗电量的关系系数
+
+BATTERY_CAPACITY_OF_DELIVERY = 100   # 送货车电池容量
+ORGAN_CAPACITY_OF_DELIVERY = 10  # 送货车器官容量
+SPEED_OF_DELIVERY = 1  # 送货车速度（时间 * 参数 = 距离）
+COST_OF_DELIVERY = 10000  # 送货车使用代价
+
+BATTERY_CAPACITY_OF_CHARGE = 1000  # 充电车电池容量
+SPEED_OF_CHARGE = 10  # 充电车速度
+COST_OF_CHARGE = 10000 # 充电车使用代价
